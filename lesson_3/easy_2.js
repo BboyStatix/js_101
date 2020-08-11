3a6d3960e257154a51201d9f7ed16b8f5819f51c
Naveed Elahi <naveed@isuntv.com>
2020-08-11 16:45:02 +0800

    Remove unused parameter

--- lesson_3/easy_2.js
+++ lesson_3/easy_2.js
@@ -83,7 +83,7 @@
 {
     let flintstones = { Fred: 0, Wilma: 1, Barney: 2, Betty: 3, Bambam: 4, Pebbles: 5 };
 
-    console.log(Object.entries(flintstones).filter(([key, val]) => key === 'Barney').pop())
+    console.log(Object.entries(flintstones).filter(([key]) => key === 'Barney').pop())
 }
 
 // Question 8
